a356f48a6d2c89088dabd59094e2e5b2502db0f6
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-03 20:47:25 +0800

    Feat: add match-case system for process_order()

--- 16.py
+++ 16.py
@@ -33,30 +33,41 @@ def resto_owner():
     elif 12.0 <= time <= 13.0:
         print(f"Resto Owner: Wait, it's {user_time}? Well I suggest you order our hottest meal just for this afternoon!")
         print("             Our Asteroid Burger with a cup of Juga Cola.")
+        return ("Asteroid Burger")
     elif 18.0 <= time <= 19.0:
         print(f"Resto Owner: Wait, it's {user_time}? Well I suggest you order our scrumptious meal just for this evening!")
         print("             Our Quantum Steak with a bucket of gravy.")
+        return ("Quantum Steak")
     else:
         print(f"Resto Owner: {user_time}? Well that's off our kitchen hours. Even then, I suggest you order our coldest drink.")
         print("             Our Pan-Galactic Gargle Blaster!")
+        return ("Pan-Galactic Gargle Blaster")
 
 def resto():
     print()
     print("Resto Owner: Greetings user! Welcome to the Space Resto!\n             May I ask what time it is? In military '##:##'.")
     x = resto_owner()
 
-    print()
     response1 = input("Resto Owner: Are you interested? (yes or no). ").strip().lower()
     if not response1 == "yes":
         raise SystemExit
     
+    print("System: Sounds great! You may proceed to the cashier to finalize your order.")
     process_order(x)
 
 def process_order(x):
-
-    
     print()
-    print(f"Resto Cashier: Hello user! The {x} morning promo is only $19.")
+    order = x
+
+    match order:
+        case "Space Omelet":
+            print(f"Resto Cashier: Good morning, user! The {order} promo is only $14.")
+        case "Asteroid Burger":
+            print(f"Resto Cashier: Good afternoon, user! The {order} meal is only $19.")
+        case "Quantum Steak":
+            print(f"Resto Cashier: Good evening, user! The {order} dine is only $29.")
+        case "Pan-Galactic Gargle Blaster":
+            print(f"Resto Cashier: Good day, user! The {order} summer refresher is only $9.")
 
 
 
