65e1e546da114885f875b3e4b0a85431691d5bec
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-04 19:59:14 +0800

    Feat: test 'for' and 'range()' in new 18.py

---
+++ 18.py
@@ -0,0 +1,20 @@
+
+
+def user_input():
+    while True:
+        number = int(input("Give me a number: "))
+        if number > 0:
+            break
+        print("Please enter a natural number.")
+    return number
+
+def output(n):
+    for _ in range(n):
+        print(f"This is said {n} amount of times.")
+    
+
+def main():
+    number = user_input()
+    output(number)
+
+main()
