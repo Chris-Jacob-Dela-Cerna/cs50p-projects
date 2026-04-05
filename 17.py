0d7f97eb65a530b501f215b85864328edc5ca4e3
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-05 19:39:43 +0800

    Feat: finish match() adding fruit-color matching with scoring system

--- 17.py
+++ 17.py
@@ -13,7 +13,7 @@ def program():
             print("Thank you for using Calculator.")
 
         elif response == "2":
-            print("\nWelcome to Match!.", end="")
+            print("\nWelcome to Match!", end="")
             # Placeholder
             match()
             print("Thank you for using Match.")
@@ -56,6 +56,31 @@ match_list = [
 
 def match():
     while True:
-        print(match_list["apple"]["red"])
+        print("\nInstructions: You will be asked a fruit and you must match it with a color.\n")
+
+        score = 0
+        ans = input(f"Question 1: {match_list[0]["name"].title()}\nUser: ").strip().lower()
+        if ans == match_list[0]["color"]:
+            score += 1
+        ans = input(f"Question 2: {match_list[2]["name"].title()}\nUser: ").strip().lower()
+        if ans == match_list[2]["color"]:
+            score += 1
+        ans = input(f"Question 3: {match_list[1]["color"].title()}\nUser: ").strip().lower()
+        if ans == match_list[1]["name"]:
+            score += 1
+        ans = input(f"Question 4: {match_list[2]["color"].title()}\nUser: ").strip().lower()
+        if ans == match_list[2]["name"]:
+            score += 1
+        ans = input(f"Question 5: {match_list[1]["name"].title()}\nUser: ").strip().lower()
+        if ans == match_list[1]["color"]:
+            score += 1
+        ans = input(f"Question 6: {match_list[0]["color"].title()}\nUser: ").strip().lower()
+        if ans == match_list[0]["name"]:
+            score += 1
+        print(f"\nCongratulations! You've got a final score or {score}/6.")
+
+        again = input("\nTry again? Enter yes or no.\nUser: ")
+        if again != "yes":
+            break
 
 program()
