2552926338a57832e9e14e43e18647036a7618db
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-05 14:42:53 +0800

    Feat: add loop to program(), prompt user if they want to exit

--- 17.py
+++ 17.py
@@ -1,15 +1,36 @@
 # Document: This python is my 1st application of CS50P Week 2.
 
 def program():
-    response = input("\nCalculator or Match? (1 or 2)\nUser: ")
-    if response == "1":
-        print("\nWelcome to Calculator! ")
-    while response == "1":
-        calculator()
-        leave = input("\nExit calculator? (yes or no)\nUser: ")
-        if leave == "yes":
+    while True:
+        response = input("\nGreetings user!\nWould you like to run calculator or match? Enter 1 or 2.\nEnter 'exit' if you want to leave.\nUser: ").strip().lower()
+        if response == "exit":
             break
-    # Match
+
+        elif response == "1":
+            print("\nWelcome to Calculator! ")
+            while True:
+                calculator()
+                leave = input("\nExit calculator? (yes or no)\nUser: ")
+                if leave == "yes":
+                    break
+
+        elif response == "2":
+            print("\nWelcome to Match! ")
+            while True:
+                print("BALLER")
+                break
+
+        else:
+            print("Input is invalid. Please try again.")
+
+# Implement Match
+
+
+
+
+
+
+
 
 
 def calculator():
@@ -18,7 +39,7 @@ def calculator():
         x, op, y = exp.split(" ")
         x = float(x)
         y = float(y)
-
+# Check if x and y is a number
         if op == "+" or op == "-" or op == "*" or op == "/":
             if op == "+":
                 print("=", x + y)
@@ -28,6 +49,7 @@ def calculator():
                 print("=", x * y)
             elif op == "/":
                 print("=", x / y)
+# Prompt user if they want to run another calculation
             break
         else:
             print("Enter a valid operator.")
