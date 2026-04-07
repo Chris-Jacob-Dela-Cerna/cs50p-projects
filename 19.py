1896c3494d21be9cb0d34f6ba05ba57b95db79cc
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-07 19:30:50 +0800

    Feat: create 19.py and make different data structures for testing

---
+++ 19.py
@@ -0,0 +1,18 @@
+# 
+
+# List for testing
+fruits = ["apple", "banana", "orange"]
+
+# List of Dictionaries for testing
+fruits_n_color = [
+    {"name": "apple", "color": "red"}
+    {"name": "banana", "color": "yellow"}
+    {"name": "orange", "color": "orange"}
+]
+
+# Nestled Dictionaries for testing
+fruits_n_color = {
+    "apple": {"name": "apple", "color": "red"},
+    "banana": {"name": "banana", "color": "yellow"},
+    "orange": {"name": "orange", "color": "orange"}
+}
