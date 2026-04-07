e67424db8c270ea210bcc3b6d0c2fe28df98d3ae
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-07 19:54:28 +0800

    Feat: add a few 'for' loop examples

--- 19.py
+++ 19.py
@@ -1,18 +1,33 @@
-# 
+# # Document: This python is my 3rd application of CS50P Week 2.
+
+''' This program will be used to note down different ways to use lists/dictionaries with the for loop.
+The goal is to better understand the "for" loop. '''
 
 # List for testing
 fruits = ["apple", "banana", "orange"]
 
 # List of Dictionaries for testing
-fruits_n_color = [
-    {"name": "apple", "color": "red"}
-    {"name": "banana", "color": "yellow"}
+fruitsncolor = [
+    {"name": "apple", "color": "red"},
+    {"name": "banana", "color": "yellow"},
     {"name": "orange", "color": "orange"}
 ]
 
-# Nestled Dictionaries for testing
-fruits_n_color = {
+# Nested Dictionaries for testing
+fruitsncolor2 = {
     "apple": {"name": "apple", "color": "red"},
     "banana": {"name": "banana", "color": "yellow"},
     "orange": {"name": "orange", "color": "orange"}
 }
+
+
+for eachfruit in fruits:
+    print(eachfruit)
+print()
+
+for eachfruit in fruitsncolor[2].keys():
+    print(eachfruit)
+print()
+
+for eachfruit in fruitsncolor2["orange"].keys():
+    print(eachfruit)
