8a7b771ca207b58fa92cd3dba1d80783b8ef85dc
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-12 14:52:54 +0800

    Feat: create a list of operators and add a calculation system

--- week-3/22.py
+++ week-3/22.py
@@ -1,14 +1,31 @@
 # Document: This python is my 1st application of CS50P Week 3.
 
+stroperators = ["+", "-", "*", "/"]
+
 while True:
     try:
         x, op, y = input("\nSystem:   In this format: [x op y].\n          Enter a mathematical expression.\nUser:     ").strip().split(" ")
     except ValueError:
         print("System:   Please enter a valid expression.")
+        continue
     else:
         try:
             x, y = int(x), int(y)
         except ValueError:
             print("System:   Please enter a valid number.")
+            continue
         else:
-            break
+            if not op in stroperators:
+                print("System:   Please enter a valid operator.")
+                continue
+            else:
+                if op == "+":
+                    expression = x + y
+                elif op == "-":
+                    expression = x - y
+                elif op == "*":
+                    expression = x * y
+                else:
+                    expression = x / y
+                break
+print(f"System:   The answer is {expression}.")
