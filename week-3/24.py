b7f2928d93db03fd6aefea08271e21f5583a91a4
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-13 14:31:25 +0800

    Feat: create cargo_inventory() function to let user input items

--- week-3/24.py
+++ week-3/24.py
@@ -2,18 +2,48 @@
 
 
 def main():
-    greet()
-    fuel_management()
+    # intro()
+    cargo_inventory()
+    # canteen()
+    # fuel_management()
+    # shipment_audit()
+
+def intro():
+    print("\nContext: You are managing a shipping hub in Metro Manila that sends Balikbayan boxes to various provinces.\nYou must manage the inventory of items being packed, the fuel for the delivery trucks, and the schedule of shipments.")
 
 
 
-def greet():
-    print("\nYou are managing a shipping hub in Metro Manila that sends Balikbayan boxes to various provinces.\nYou must manage the fuel for the delivery trucks, the inventory of items being packed,\nand the schedule of shipments.")
+def cargo_inventory():
+    print("\nYou head to the packing station. You are tasked to pack items inside a Balikbayan Box.")
+    while True:
+        try:    
+            useritem = input("System:   Enter the name of the items 1 by 1.\n          Enter ctrl-z to finish packing items.").strip()
+        except EOFError:
+            inventory()
+            break
+        else:
+            insert_item()
+
+def inventory():
+    pass
+
+def insert_item():
+    pass
+
+
+
+def canteen():
+    pass
 
 
 
 def fuel_management():
-    print()
+    pass
+
+
+
+def shipment_audit():
+    pass
 
 
 
