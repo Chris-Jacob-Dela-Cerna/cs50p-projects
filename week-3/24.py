1eb7f9b877d27021a664856b5b8b8c88c90e158e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-13 13:54:59 +0800

    Feat: start W-3 Master Challenge. Add structure for program

---
+++ week-3/24.py
@@ -0,0 +1,20 @@
+# Document: This python is my last application of CS50P Week 3.
+
+
+def main():
+    greet()
+    fuel_management()
+
+
+
+def greet():
+    print("\nYou are managing a shipping hub in Metro Manila that sends Balikbayan boxes to various provinces.\nYou must manage the fuel for the delivery trucks, the inventory of items being packed,\nand the schedule of shipments.")
+
+
+
+def fuel_management():
+    print()
+
+
+
+main()
