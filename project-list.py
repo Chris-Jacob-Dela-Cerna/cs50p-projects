26a2573eedc40b1f840dbe008ebcd2aa3bebc18b
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-15 22:13:55 +0800

    Feat: improve project-list.py with nested lists inside a list and applying DRY principle

--- project-list.py
+++ project-list.py
@@ -1,6 +1,5 @@
 # This python will be used to track my .py projects.
-
-week0 = [
+weeks = [[
     "Simple conversation",
     "Trial conversation",
     "Simple 2-constant calculator",
@@ -12,44 +11,29 @@ week0 = [
     "Simple inside voice converter",
     "Simple word counter",
     "Master Challenge - Einstein's Formatter"
-]
-
-week1 = [
+], [
     "Simple odd or even checker",
     "10-item Quiz",
     "Simple color sorter",
     "Game recommender",
     "Master Challenge - Hitchhiker's Digital Cafe"
-]
-
-week2 = [
+], [
     "Calculator or match",
     "Simple fruit dictionary",
     "Lists and dictionaries",
     "Simple word slicer",
     "Master Challenge - LRT-2 Digital Commuter Hub" 
-]
-
-week3 = [
+], [
     "Better Calculator",
     "Simple Fruit Guesser",
     "Master Challenge - Balikbayan Logistics"
-]
+]]
 
 print("\nFull Project List")
 
-print("\nWeek 0:")
-for eachproject in range(len(week0)):
-    print(f"{eachproject + 1}.", week0[eachproject])
+for eachweek in range(len(weeks)):
+    print(f"\nWeek {eachweek}:")
+    for eachproject in range(len(weeks[eachweek])):
+        print(f"{eachproject + 1}.", weeks[eachweek][eachproject])
 
-print("\nWeek 1:")
-for eachproject in range(len(week1)):
-    print(f"{eachproject + 1}.", week1[eachproject])
-
-print("\nWeek 2:")
-for eachproject in range(len(week2)):
-    print(f"{eachproject + 1}.", week2[eachproject])
-
-print("\nWeek 3:")
-for eachproject in range(len(week3)):
-    print(f"{eachproject + 1}.", week3[eachproject])
+print()
