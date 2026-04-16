ab80e10c1f335506a3df223bbf82b684d220403d
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-16 14:23:14 +0800

    Feat: create a guess() function that works for both games

--- week-4/25.py
+++ week-4/25.py
@@ -5,7 +5,7 @@ import random
 
 
 def main():
-    print("System:   Would you like to guess a number or a fruit? Enter 1 or 2.\n          Enter 'ctrl-z' to leave.")
+    print("\nSystem:   Would you like to guess a number or a fruit? Enter 1 or 2.\n          Enter 'ctrl-z' to leave.")
     while True:
         try:
             whichgame = input("User:     ").strip().lower()
@@ -14,25 +14,38 @@ def main():
             break
         else:
             if whichgame == "1":
-                game = "number"
+                game = numbers
                 userguess = get_number()
             elif whichgame == "2":
-                game = "fruit"
+                game = fruits
             else:
                 print("System:   Invalid. Please try again.")
                 continue
             guess(game, userguess)
+            break
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+fruits = []
 
 def get_number():
     while True:
         try:
-            userguess = int(input("\nSystem:   Enter a number from 1-10.\nUser:     ").strip())
+            userguess = int(input("\nSystem:   Guess a number from 1-10.\nUser:     ").strip())
         except ValueError:
             print("System:   Invalid. Please enter a valid number.")
         else:
-            return userguess
-
+            if userguess in numbers:
+                return userguess
+            else:
+                print("System:   Invalid. Please enter a number from 1-10.")
+            
 def guess(game, userguess):
-    pass
+    correct = random.choice(game)
+    print(f"System:   The answer was {correct}.", end=" ")
+    if userguess == correct:
+        print("You won, congratulations!\n")
+    else:
+        print("You lost, better luck next time!\n")
 
 main()
