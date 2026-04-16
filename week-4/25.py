982b05a059800ce392b7fba37b6bc376d5f7fb0c
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-16 14:36:03 +0800

    Feat: create get_fruit() with dialogue, improve dialogue and flow of program

--- week-4/25.py
+++ week-4/25.py
@@ -3,7 +3,6 @@
 import random
 
 
-
 def main():
     print("\nSystem:   Would you like to guess a number or a fruit? Enter 1 or 2.\n          Enter 'ctrl-z' to leave.")
     while True:
@@ -18,20 +17,24 @@ def main():
                 userguess = get_number()
             elif whichgame == "2":
                 game = fruits
+                userguess = get_fruit()
             else:
                 print("System:   Invalid. Please try again.")
                 continue
             guess(game, userguess)
             break
 
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-fruits = []
+fruits = ["Apple", "Banana", "Orange", "Grapes", "Strawberry", "Pineapple", "Mango", "Watermelon", "Blueberry", "Peach"]
+
 
 def get_number():
+    print("\nSystem:   Guess a number from 1-10.")
     while True:
         try:
-            userguess = int(input("\nSystem:   Guess a number from 1-10.\nUser:     ").strip())
+            userguess = int(input("User:     ").strip())
         except ValueError:
             print("System:   Invalid. Please enter a valid number.")
         else:
@@ -39,13 +42,27 @@ def get_number():
                 return userguess
             else:
                 print("System:   Invalid. Please enter a number from 1-10.")
-            
+
+
+def get_fruit():
+    print("\nSystem:   Guess a fruit from the following fruits below:")
+    for eachfruit in fruits:
+        print(f" | {eachfruit.title()}")
+    while True:
+        userguess = input("User:     ").strip().title()
+        if userguess in fruits:
+            return userguess
+        else:
+            print("System:   Invalid. Please enter a valid fruit.")
+
+
 def guess(game, userguess):
     correct = random.choice(game)
-    print(f"System:   The answer was {correct}.", end=" ")
+    print(f"System:   The answer was {correct.lower()}.", end=" ")
     if userguess == correct:
         print("You won, congratulations!\n")
     else:
         print("You lost, better luck next time!\n")
+
 
 main()
