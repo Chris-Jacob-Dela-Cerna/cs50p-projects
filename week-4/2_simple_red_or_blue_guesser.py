bc0b62c8ba1e01637d8dd5f2a9e85bd5edecaf2a
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-17 21:37:12 +0800

    Feat: replace else with an if statement using sys.exit()

--- week-4/2_simple_red_or_blue_guesser.py
+++ week-4/2_simple_red_or_blue_guesser.py
@@ -3,15 +3,16 @@
 import sys
 import random
 
+
+if len(sys.argv) != 2:
+    sys.exit(f"System:   Invalid. Please enter either red or blue.\n          >>> python {sys.argv[0]} [input] <<<")
+
 colors = ["red", "blue"]
 
-if len(sys.argv) == 2:
-    print(f"System:   User entered '{sys.argv[1]}'.")
-    win = random.choice(colors)
-    if sys.argv[1].lower() == win:
-        print("System:   You win!", end=" ")
-    else:
-        print("System:   You lost!", end=" ")
-    print(f"The answer was {win}.")
+print(f"System:   User entered '{sys.argv[1]}'.")
+win = random.choice(colors)
+if sys.argv[1].lower() == win:
+    print("System:   You win!", end=" ")
 else:
-    print(f"System:   Invalid. Please enter either red or blue.\n          >>> python {sys.argv[0]} [input] <<<")
+    print("System:   You lost!", end=" ")
+print(f"The answer was {win}.")
