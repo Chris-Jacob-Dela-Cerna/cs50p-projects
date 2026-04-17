76fa1f8917702027b462bfc3457111b6b657f493
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-17 21:52:04 +0800

    Feat: add a full name input checker to use the user's full name

--- week-4/2_simple_red_or_blue_guesser.py
+++ week-4/2_simple_red_or_blue_guesser.py
@@ -3,11 +3,12 @@
 import sys
 import random
 
-
-if len(sys.argv) != 2:
-    sys.exit(f"System:   Invalid. Please enter either red or blue.\n          >>> python {sys.argv[0]} [input] <<<")
-
 colors = ["red", "blue"]
+
+if len(sys.argv) == 1:
+    sys.exit(f"System:   Invalid. Please enter either red or blue.\n          >>> python {sys.argv[0]} [input] <<<")
+if len(sys.argv) == 2:
+    sys.exit(f"System:   Invalid. Please enter your full name.\n          >>> python {sys.argv[0]} [input] [\"fullname\"] <<<")
 
 print(f"System:   User entered '{sys.argv[1]}'.")
 win = random.choice(colors)
@@ -15,4 +16,4 @@ if sys.argv[1].lower() == win:
     print("System:   You win!", end=" ")
 else:
     print("System:   You lost!", end=" ")
-print(f"The answer was {win}.")
+print(f"The answer was {win}.\n          Thank you for playing {sys.argv[2].title()}.")
