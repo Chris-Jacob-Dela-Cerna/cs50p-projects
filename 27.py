91d5e50bcfbbe61e7cbab4c2dd197b4393b837c1
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-18 21:24:28 +0800

    Feat: create a simple structure and dialogue

--- 27.py
+++ 27.py
@@ -2,10 +2,22 @@
 
 import requests
 
-
-characters = requests.get("https://dragonball-api.com/api/characters")
-
+characters = requests.get("https://dragonball-api.com/api/characters?limit=58")
 store = characters.json()
 
-for eachid in range(len(store["items"])):
-    print(eachid + 1, store["items"][eachid].get("name"))
+def main():
+    start()
+
+def start():
+    userinput = input("System:   Enter 'characters' to see the list of characters.\nUsers:    ").strip().lower()
+    if userinput == "characters":
+        character_list()
+    else:
+        pass
+
+def character_list():
+    print("\nList of Characters:")
+    for eachid in range(len(store["items"])):
+        print(eachid + 1, store["items"][eachid].get("name"))
+
+main()
