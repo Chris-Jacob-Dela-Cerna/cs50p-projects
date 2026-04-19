c878d51876c76039c267d7e181719cb54d26349e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-19 10:29:15 +0800

    Feat: include a EOFError checker in character_ui and a id checker in start()

--- week-4/27.py
+++ week-4/27.py
@@ -5,26 +5,36 @@ import requests
 characters = requests.get("https://dragonball-api.com/api/characters?limit=58")
 api = characters.json()
 
+idlist = []
+for number in range(58):
+    idlist.append(str(number))
+
 def main():
     start()
 
 def start():
-    userinput = input("System:   Enter 'characters' to see the list of characters.\nUsers:    ").strip().lower()
-    if userinput == "characters":
-        character_ui()
-    else:
-        pass
+    while True:
+        userinput = input("\nSystem:   Enter a characterid to see their info.\n          Enter 'characters' to see the list of characterids.\nUsers:    ").strip().lower()
+        if userinput == "characters":
+            character_ui()
+        else:
+            if userinput in idlist:
+                pass
+            else:
+                print(f"System:   characterid [{userinput}] is invalid.")
 
 def character_ui():
     userpage = 0
     while True:
         uipage = userpage + 1
-        if 0 <= userpage < 6:
+        if 0 <= userpage <= 5:
             character_list(userpage)
             try:
                 userpage = int(input(f"Current page: {uipage}/6\n>>> ").strip())
             except ValueError:
                 continue
+            except EOFError:
+                break
             else:
                 userpage = userpage - 1
         else:
