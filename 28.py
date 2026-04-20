85185b7e8a8bbddae49b09cf41011471bb4546a6
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-20 21:59:15 +0800

    Feat: create connection checker for coincap api

--- 28.py
+++ 28.py
@@ -1,13 +1,15 @@
 # Document: This python is my last application of CS50P Week 4.
 
 import sys
+import requests
+
 
 
 def main():
-    if check_net:
-        pass
-    else:
+    if check_net():
+        print("System:   [Connection timeout]")
         sys.exit()
+    print("System:   [Connection established]")
 
     if check_args() == 0:
         pass
@@ -17,10 +19,18 @@ def main():
         pass
 
 def check_net():
-    pass
+    try:
+        bitcoin_api = requests.get(
+            "https://rest.coincap.io/v3/assets/bitcoin?apiKey=ac615c2394516854ececfaa073631bf60b40a36236294c491470d41a0770595a"
+            )
+        bitcoin_api.raise_for_status()
+    except requests.RequestException:
+        return True
+
 
 def check_args():
     pass
+
 
 if __name__ == "__main__":
     main()
