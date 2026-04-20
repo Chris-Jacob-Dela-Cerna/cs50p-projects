12e461f18f2759d621cbf6923848d44ff46ade43
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-20 21:27:59 +0800

    Feat: start W-4 Master Challenge. Add structure for program

---
+++ 28.py
@@ -0,0 +1,26 @@
+# Document: This python is my last application of CS50P Week 4.
+
+import sys
+
+
+def main():
+    if check_net:
+        pass
+    else:
+        sys.exit()
+
+    if check_args() == 0:
+        pass
+    elif check_args() == 1:
+        pass
+    else:
+        pass
+
+def check_net():
+    pass
+
+def check_args():
+    pass
+
+if __name__ == "__main__":
+    main()
