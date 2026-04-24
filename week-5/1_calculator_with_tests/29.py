08a1521fe16e7b17cc2e12d1363b6e1877ce9ad9
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-24 21:25:25 +0800

    Feat: create a testable function like multiply() to test later

---
+++ week-5/1_calculator_with_tests/29.py
@@ -0,0 +1,28 @@
+# Document: This python is my 1st application of CS50P Week 5.
+
+def main():
+    print(
+        "\nLet's do some multiplication!"
+        "\nEnter two numbers [x y]."
+    )
+    x, y = get_numbers()
+    print("Answer:", multiply(x, y))
+
+
+def get_numbers():
+    while True:
+        try:
+            x, y = input("User: ").strip().split(" ")
+            x, y = int(x), int(y)
+        except ValueError:
+            print("Invalid numbers.")
+        else:
+            return x, y
+
+
+def multiply(x, y):
+    return x * y
+
+
+if __name__ == "__main__":
+    main()
