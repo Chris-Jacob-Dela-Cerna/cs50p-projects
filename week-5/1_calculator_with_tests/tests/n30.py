9805bfd0044f273c2cce6b8cb48cc844fd24a47e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-24 22:34:20 +0800

    Feat: create 4 operator tests in n30.py in tests folder

--- week-5/1_calculator_with_tests/tests/n30.py
+++ week-5/1_calculator_with_tests/tests/n30.py
@@ -2,5 +2,15 @@
 
 from calculator import n29
 
+
+def test_addtion():
+    assert n29.addition(3, 7) == 10
+
+def test_subtraction():
+    assert n29.subtraction(5, 3) == 2
+
 def test_multiply():
     assert n29.multiply(3, 3) == 9
+
+def test_divide():
+    assert n29.divide(6, 3) == 9
