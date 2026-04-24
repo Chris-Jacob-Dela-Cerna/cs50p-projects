06184ece7d9afe4a08ba4ecd9e094962f198bf50
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-24 22:31:57 +0800

    Feat: create 4 operators for n29.py, and match case for idetifying correct operation

--- week-5/1_calculator_with_tests/calculator/n29.py
+++ week-5/1_calculator_with_tests/calculator/n29.py
@@ -2,26 +2,47 @@
 
 def main():
     print(
-        "\nLet's do some multiplication!"
-        "\nEnter two numbers [x y]."
+        "\nLet's do some calculation!"
+        "\nEnter two numbers [x op y]."
     )
-    x, y = get_numbers()
-    print("Answer:", multiply(x, y))
+    x, op, y = get_numbers()
+    match op:
+        case "+":
+            answer = addition(x, y)
+        case "-":
+            answer = subtraction(x, y)
+        case "*":
+            answer = multiply(x, y)
+        case "/":
+            answer = divide(x, y)
+    print("Answer:", answer)
 
+operators = ["+", "-", "*", "/"]
 
 def get_numbers():
     while True:
         try:
-            x, y = input("User: ").strip().split(" ")
+            x, op, y = input("User: ").strip().split(" ")
             x, y = int(x), int(y)
+            if op not in operators:
+                raise ValueError
         except ValueError:
-            print("Invalid numbers.")
+            print("Invalid expression.")
         else:
-            return x, y
+            return x, op, y
 
+
+def addition(x, y):
+    return x + y
+
+def subtraction(x, y):
+    return x - y
 
 def multiply(x, y):
     return x + y
+
+def divide(x, y):
+    return x / y
 
 
 if __name__ == "__main__":
