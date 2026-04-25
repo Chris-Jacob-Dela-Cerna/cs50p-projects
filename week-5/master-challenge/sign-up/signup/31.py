2371d539c546236b90cfb9ab4aa123a2ec09b7e3
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-25 21:50:21 +0800

    Feat: create initial dialogue for main()

---
+++ week-5/master-challenge/sign-up/signup/31.py
@@ -0,0 +1,31 @@
+# Document: This python is my last application of CS50P Week 5.
+
+def main():
+    print(
+        "\n  ==================="
+        "\n   Sign-up and login"
+        "\n  ==================="
+        "\n\nSystem:   Enter your display name to create a username."
+    )
+    display_name = input("User:     ").strip()
+    username(display_name)
+
+
+def username(display_name):
+    pass
+
+
+def verify_username():
+    pass
+
+
+def password():
+    pass
+
+
+def verify_password():
+    pass
+
+
+if __name__ == "__main__":
+    main()
