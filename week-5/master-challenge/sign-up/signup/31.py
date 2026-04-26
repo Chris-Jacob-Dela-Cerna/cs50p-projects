d43ff3df45f275e892754f1442f08e9328ac8127
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-26 12:28:24 +0800

    Feat: create verify_username() for validating username characters with return

--- week-5/master-challenge/sign-up/signup/31.py
+++ week-5/master-challenge/sign-up/signup/31.py
@@ -8,22 +8,38 @@ def main():
         "\n\nSystem:   Enter your display name to create a username."
     )
     display_name = input("User:     ")
-    name = username(display_name)
-    verify_username(name)
+    user_name = username(display_name)
+
+    status = verify_username(user_name)
+    if not status:
+        print("System:   Invalid input.")
     
 
 def username(display_name):
     vowels = "aeiouAEIOU"
-    name = ""
+    user_name = ""
     for character in display_name.strip():
         if character not in vowels:
-            name += character
-    return name
+            user_name += character
+    return user_name
 
 
-def verify_username():
-    pass
-
+def verify_username(user_name):
+    for character in user_name:
+        if not character.isalnum():
+            return False
+    for number in range(3):
+        if not user_name[number].isalpha():
+            return False
+    digit = 0
+    for number in range(len(user_name)):
+        if user_name[number].isdigit():
+            digit = number
+            break
+    if digit != 0:
+        if not user_name[digit:].isdigit():
+            return False
+    return True
 
 def password():
     pass
