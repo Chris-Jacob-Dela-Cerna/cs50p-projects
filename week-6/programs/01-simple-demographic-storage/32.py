c967dae111fbc47ef22e92e463b8f3cbe391581a
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-28 21:13:48 +0800

    Feat: create list of demographic questions

--- week-6/programs/01-simple-demographic-storage/32.py
+++ week-6/programs/01-simple-demographic-storage/32.py
@@ -1,2 +1,12 @@
 # Document: This python is my 1st application of CS50P Week 6.
 
+questions = {
+    "Age": "What's your age?",
+    "Gender": "Are you a male or female?",
+    "Location": "Where are you currently located?",
+    "Education Level": "What is the highest education you've attained?",
+    "Employment Status": "Are you currently employed or no?",
+}
+
+for question in questions:
+    demo = input(f"System:   {question}").strip().title()
