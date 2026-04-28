273710d29d15c591e4e5c2027a5865ebebc0479e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-28 21:35:52 +0800

    Feat: add a formatter function to return into the external txt file

--- week-6/programs/01-simple-demographic-storage/32.py
+++ week-6/programs/01-simple-demographic-storage/32.py
@@ -8,10 +8,20 @@ questions = {
     "Employment Status": "Are you currently employed or no?",
 }
 
-user = {}
-for demographic, question in questions.items():
-    user_demo = input(f"System:   {question}").strip().title()
-    user.update({demographic: user_demo})
+
+user = []
+for demo, question in questions.items():
+    result = input(f"System:   {question} ").strip().title()
+    user.append({"demographic": demo, "user_demo": result})
+
+
+def format_data():
+    return f"""{user[0]['demographic']}: {user[0]["user_demo"]}
+{user[1]['demographic']}: {user[1]["user_demo"]}
+{user[2]['demographic']}: {user[2]["user_demo"]}
+{user[3]['demographic']}: {user[3]["user_demo"]}
+{user[4]['demographic']}: {user[4]["user_demo"]}"""
+
 
 with open("recent_demo.txt", "w") as stored:
-    stored.write()
+    stored.write(format_data())
