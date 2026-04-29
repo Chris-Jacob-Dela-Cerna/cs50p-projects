d621e3d173758dafa301d620653d82cbb16a896d
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-29 22:17:17 +0800

    Feat: create dialogue for 33.py 'Quizpin' and initial structure

--- week-6/programs/02-33/33.py
+++ week-6/programs/02-33/33.py
@@ -1,10 +1,26 @@
 # Document: This python is my 2nd application of CS50P Week 6.
 
-while True:
-    try:
-        with open("subject_data.txt") as data:
-            print(data.readlines()[0])
-        break
-    except FileNotFoundError:
-        with open("subject_data.txt", "w") as data:
-            data.write("yes file")
+
+def main():
+    input(
+        "  ====================="
+        "\n   Welcome to Quizpin!"
+        "\n  =====================\n"
+    )
+    print(
+        "Quizpin:"
+        "\n | a - create a quiz"
+        "\n | b - start a quiz"
+    )
+    mode = input("User:   ")
+
+
+def insert_data():
+    pass
+
+
+def quiz():
+    pass
+
+
+main()
