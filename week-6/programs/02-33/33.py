a1981ec1af4435e56323c4acb1395b83895e0f60
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-29 22:43:24 +0800

    Feat: create user_mode validator

--- week-6/programs/02-33/33.py
+++ week-6/programs/02-33/33.py
@@ -2,24 +2,43 @@
 
 
 def main():
-    input(
-        "  ====================="
-        "\n   Welcome to Quizpin!"
-        "\n  =====================\n"
+    print(
+        "====================="
+        "\n Welcome to Quizpin!"
+        "\n====================="
     )
+    input()
     print(
         "Quizpin:"
         "\n | a - create a quiz"
         "\n | b - start a quiz"
     )
-    mode = input("User:   ")
+    while True:
+        user_mode = input("User:   ")
+        mode = check_mode(user_mode)
+        if mode == "Create Quiz":
+            create_quiz()
+            break
+        elif mode == "Start Quiz":
+            start_quiz()
+            break
 
 
-def insert_data():
+def check_mode(user_mode):
+    mode = user_mode.strip().lower()
+    if mode == "a":
+        return "Create Quiz"
+    elif mode == "b":
+        return "Start Quiz"
+    else:
+        return False
+
+
+def create_quiz():
     pass
 
 
-def quiz():
+def start_quiz():
     pass
 
 
