294175562ca7d431edfd12059294cbf592bf934b
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-04-29 22:02:15 +0800

    Try: try using open() through try/except and loops

--- week-6/programs/02-33/33.py
+++ week-6/programs/02-33/33.py
@@ -1,2 +1,10 @@
 # Document: This python is my 2nd application of CS50P Week 6.
 
+while True:
+    try:
+        with open("subject_data.txt") as data:
+            print(data.readlines()[0])
+        break
+    except FileNotFoundError:
+        with open("subject_data.txt", "w") as data:
+            data.write("yes file")
