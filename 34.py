9d0e01482f334c9d9fe4b738a55ffccf4599ca2d
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-01 10:37:49 +0800

    Feat: improve structure of week-6 and rename accordingly

--- 34.py
+++
@@ -1,9 +0,0 @@
-
-fruits = []
-for number in range(3):
-    user_fruit = input("\nFruit: ")
-    user_color = input("Color: ")
-    fruits.append({"fruit": user_fruit, "color": user_color})
-
-for fruit in sorted(fruits, key=lambda item: item['fruit']):
-    print(fruit["fruit"])
