578e5f120eb2483e094d144b9b519e798251b6f7
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-05 20:44:15 +0800

    Feat: add a quit option in choosing a mode

--- week-6/programs/02-quizpin/quizpin/main.py
+++ week-6/programs/02-quizpin/quizpin/main.py
@@ -1,5 +1,6 @@
 # Document: This python is my 2nd application of CS50P Week 6.
 
+import sys
 from create_items import create_items
 from start_quiz import start_quiz
 from checker import checker
@@ -17,6 +18,7 @@ def main():
         modes = {
             "a": "Create Quiz Items",
             "b": "Start a Quiz",
+            "d": "Quit",
         }
         print("Quizpin:  Choose a mode:")
         for letter, mode in modes.items():
@@ -34,6 +36,16 @@ def main():
             create_items()
         elif result == modes["b"]:
             start_quiz()
+        elif result == modes["d"]:
+            print(
+                "Quizpin:  Thank you for using Quizpin."
+                "\n"
+                "\n============"
+                "\n Goodbye :)"
+                "\n============"
+                "\n"
+            )
+            sys.exit()
 
 
 if __name__ == "__main__":
