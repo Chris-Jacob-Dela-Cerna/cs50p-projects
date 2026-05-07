c30aea8475b0bd39c72aa3858ab546d627af119d
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-07 15:39:19 +0800

    Feat: add a dictionary checking system for tools

--- week-6/programs/02-quizpin/quizpin/manage_quizzes.py
+++ week-6/programs/02-quizpin/quizpin/manage_quizzes.py
@@ -15,15 +15,37 @@ def manage_quizzes():
         "\nQuizpin:  Type r-[number] to rename the quiz."
         "\n          Type d-[number] to delete the quiz."
         "\n          Example: r-2"
-        "\nL. List of Quizzes:"
-        "\n"
     )
 
     number = 0
     quiz_list = {}
+    print("\nList of Quizzes:")
     for quiz in quizzes:
         number += 1
+        quiz_name = quiz[:-4].replace("_", " ")
         quiz_list.update({str(number): quiz})
-        print(f"{number}) {quiz}")
-    
-    input()
+        print(f"{number}) {quiz_name}")
+
+    tools = {
+        "r-": "rename",
+        "d-": "delete",
+    }
+    while True:
+        user = input("\nUser:     ")
+        result = check_manage(user, tools)
+        if result:
+            break
+
+    if result == tools["r-"]:
+        pass
+    elif result == tools["d-"]:
+        pass
+
+
+def check_manage(user, tools):
+    chosen = user.strip().lower()
+    for key, tool in tools.items():
+        if chosen.startswith(key):
+            return tool
+    else:
+        return None
