cafe8966ac16cf7406a38efcd16dc5325e72c7cf
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-11 13:44:04 +0800

    Feat: add a file validation guard for count.py

--- week-6/master-challenge/src/count.py
+++ week-6/master-challenge/src/count.py
@@ -1,4 +1,14 @@
 
 
+import sys
+
+
 def count_lines():
-    pass
+    try:
+        program = sys.argv[2]
+    except IndexError:
+        print("[Error  :  System] No python program selected.")
+    else:
+        if not program.endswith(".py"):
+            print("[Error  :  System] Invalid file.")
+            sys.exit()
