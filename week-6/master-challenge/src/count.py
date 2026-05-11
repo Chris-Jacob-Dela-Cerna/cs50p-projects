adacbe6a0959e2b91e5ae63c2ff7894c4854fb8f
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-11 14:12:00 +0800

    Feat: reformat current code structure to be more pythonic and clean

--- week-6/master-challenge/src/count.py
+++ week-6/master-challenge/src/count.py
@@ -4,11 +4,26 @@ import sys
 
 
 def count_lines():
-    try:
-        program = sys.argv[2]
-    except IndexError:
+    file = retrieve_file()
+    if not file:
         print("[Error  :  System] No python program selected.")
+        sys.exit()
+    program = validate_file(file)
+    if not program:
+        print("[Error  :  System] Invalid file.")
+        sys.exit()
+
+
+def retrieve_file():
+    try:
+        file = sys.argv[2]
+    except IndexError:
+        return None
     else:
-        if not program.endswith(".py"):
-            print("[Error  :  System] Invalid file.")
-            sys.exit()
+        return file
+    
+
+def validate_file(program):
+    if not program.endswith(".py"):
+        return None
+    return program
