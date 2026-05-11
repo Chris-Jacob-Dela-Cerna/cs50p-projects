67fcec26517e625af6863c149958ce8a6e12b570
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-11 14:23:01 +0800

    Feat: use os to access samples folder in data, and extract list

--- week-6/master-challenge/src/count.py
+++ week-6/master-challenge/src/count.py
@@ -1,9 +1,11 @@
 
 
+import os
 import sys
 
 
 def count_lines():
+    access_samples()
     file = retrieve_file()
     if not file:
         print("[Error  :  System] No python program selected.")
@@ -13,6 +15,14 @@ def count_lines():
         print("[Error  :  System] Invalid file.")
         sys.exit()
 
+
+def access_samples():
+    src_path = os.path.dirname(os.path.abspath(__file__))
+    root_path = os.path.dirname(src_path)
+    smpls_path = os.path.join(root_path, "data", "samples")
+    os.makedirs(smpls_path, exist_ok=True)
+    smpls_list = os.listdir(smpls_path)
+    
 
 def retrieve_file():
     try:
