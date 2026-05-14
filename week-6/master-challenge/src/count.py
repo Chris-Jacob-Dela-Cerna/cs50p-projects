66c88b75f05021a8cf14ab1ed6980611a260f142
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-14 09:09:58 +0800

    Feat: rename variables and add ext parameter for reusablitiy

--- week-6/master-challenge/src/count.py
+++ week-6/master-challenge/src/count.py
@@ -7,7 +7,7 @@ from utils import validation as val
 
 def count_lines():
     smpls_path, smpls_list = val.access_dir("samples")
-    program = validate_file(smpls_list)
+    program = validate_file(2, ".py", smpls_list)
 
     prgm_path = os.path.join(smpls_path, program)
     file = read_lines(prgm_path)
@@ -17,22 +17,22 @@ def count_lines():
     print(f"[Success:  System] Total line count: {num}")
 
 
-def validate_file(list_):
-    file = val.retrieve_sys(2)
+def validate_file(idx, ext, list_):
+    file = val.retrieve_sys(idx)
     if not file:
         io.abort("No file selected.")
 
-    if val.retrieve_sys(3):
+    if val.retrieve_sys(idx + 1):
         io.abort("Too many arguments.")
-    
-    valid_file = val.validate_extension(file, ".py")
-    if not valid_file:
+
+    valid_file_ext = val.validate_extension(file, ext)
+    if not valid_file_ext:
         io.abort("Invalid file.")
 
-    program = val.check_file(valid_file, list_)
-    if not program:
+    valid_file = val.check_file(valid_file_ext, list_)
+    if not valid_file:
         io.abort("File does not exist.")
-    return program
+    return valid_file
 
 
 def read_lines(path):
