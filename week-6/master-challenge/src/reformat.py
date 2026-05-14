973ab3ec47a34414ed3fc1251f9c8f148cf3a0bf
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-14 20:40:49 +0800

    Feat: add a iteration to seperate last and first name and append to formatted list

--- week-6/master-challenge/src/reformat.py
+++ week-6/master-challenge/src/reformat.py
@@ -13,7 +13,12 @@ def reformat_names():
 
     old_path = os.path.join(csvs_path, old_file)
     new_path = os.path.join(csvs_path, new_file)
+
     with open(old_path, "r") as old:
         reader = csv.DictReader(old)
-        for line in reader:
-            print(line)
+        formatted = []
+        for row in reader:
+            last, first = row["name"].split(", ")
+            affiliation = row["affiliation"]
+            formatted.append({"First Name": first, "Last Name": last, "affiliation": affiliation})
+    return formatted
