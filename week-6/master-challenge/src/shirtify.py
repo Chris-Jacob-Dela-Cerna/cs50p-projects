0cc6e2a606a41f1601f588a6625eb0ff0b7c76ec
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-14 21:53:05 +0800

    Feat: add initial structure for file checking in shirtify with strict extension

--- week-6/master-challenge/src/shirtify.py
+++ week-6/master-challenge/src/shirtify.py
@@ -1,4 +1,10 @@
 
 
+from utils import args
+from utils import validation as val
+
+
 def shirtify():
-    pass
+    imgs_path, imgs_list = val.access_dir("images")
+    old_file, ext = args.validate_file(2, 4, [".jpg", ".jpeg", ".png"], True, True, imgs_list)
+    new_file = args.validate_file(3, 4, [ext], False, False, imgs_list)
