3266af9ba9d0527647edd8e279003847f31870e8
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-15 18:44:57 +0800

    Feat: establish file paths for PIL editing

--- week-6/master-challenge/src/shirtify.py
+++ week-6/master-challenge/src/shirtify.py
@@ -1,6 +1,8 @@
 
 
-import PIL
+import os
+from PIL import Image
+from PIL import ImageOps
 from utils import args
 from utils import validation as val
 
@@ -9,5 +11,13 @@ def shirtify():
     imgs_path, imgs_list = val.access_dir("images")
     old_file, ext = args.validate_file(2, 5, [".jpg", ".jpeg", ".png"], True, True, imgs_list)
     new_file = args.validate_file(3, 5, [ext], False, False, imgs_list)
-    shirt = args.validate_file(4, 5, ["shirt_.png"], False, True, imgs_list)
+    shirt_file = args.validate_file(4, 5, ["shirt_.png"], False, True, imgs_list)
 
+    old_path = os.path.join(imgs_path, old_file)
+    new_path = os.path.join(imgs_path, new_file)
+    shirt_path = os.path.join(imgs_path, shirt_file)
+
+    old = Image.open(old_path)
+    shirt = Image.open(shirt_path)
+    old.paste(shirt)
+    
