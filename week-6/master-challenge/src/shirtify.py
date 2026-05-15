821eca28ec3e6494f6f63bedfb6edd8a71598774
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-15 18:18:08 +0800

    Feat: add alien.png for demo and strict extension for shirt using 'shirt_.png'

--- week-6/master-challenge/src/shirtify.py
+++ week-6/master-challenge/src/shirtify.py
@@ -7,7 +7,7 @@ from utils import validation as val
 
 def shirtify():
     imgs_path, imgs_list = val.access_dir("images")
-    old_file, ext = args.validate_file(2, 4, [".jpg", ".jpeg", ".png"], True, True, imgs_list)
-    new_file = args.validate_file(3, 4, [ext], False, False, imgs_list)
+    old_file, ext = args.validate_file(2, 5, [".jpg", ".jpeg", ".png"], True, True, imgs_list)
+    new_file = args.validate_file(3, 5, [ext], False, False, imgs_list)
+    shirt = args.validate_file(4, 5, ["shirt_.png"], False, True, imgs_list)
 
-    
