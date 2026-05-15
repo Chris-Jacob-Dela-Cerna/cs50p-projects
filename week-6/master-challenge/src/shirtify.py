428316912fdd20c6506f08d745b01b0c8f5c839b
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-15 17:53:16 +0800

    Feat: add a white_polo.png in images and import PIL

--- week-6/master-challenge/src/shirtify.py
+++ week-6/master-challenge/src/shirtify.py
@@ -1,5 +1,6 @@
 
 
+import PIL
 from utils import args
 from utils import validation as val
 
@@ -8,3 +9,5 @@ def shirtify():
     imgs_path, imgs_list = val.access_dir("images")
     old_file, ext = args.validate_file(2, 4, [".jpg", ".jpeg", ".png"], True, True, imgs_list)
     new_file = args.validate_file(3, 4, [ext], False, False, imgs_list)
+
+    
