b0c03b81fb4d6c01591eebc21f697dad3dcb5105
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-16 19:56:54 +0800

    Feat: add email guard using re.search filter

--- week-7/programs/2.py
+++ week-7/programs/2.py
@@ -2,10 +2,20 @@
 
 import re
 
+
 while True:
     number = input("Enter your phone number: ").strip()
 
     if re.search(r"^[0123456789]{4}-[0123456789]{3}-[0123456789]{4}$", number):
+        print("Valid")
+        break
+    else:
+        print("Invalid")
+
+while True:
+    email = input("Enter your email: ").strip()
+
+    if re.search(r"^[^@]+@gmail\.com$", email):
         print("Valid")
     else:
         print("Invalid")
