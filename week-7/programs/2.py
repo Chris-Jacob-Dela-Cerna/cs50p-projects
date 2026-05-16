0ab1c9c76cc03fda8d0f71c1a4862c839c966404
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-16 20:22:32 +0800

    Feat: experiment on range values using {}

--- week-7/programs/2.py
+++ week-7/programs/2.py
@@ -4,6 +4,24 @@ import re
 
 
 while True:
+    user = input("Enter a 5-digit number: ").strip()
+
+    if re.search(r"^[1234567890]{5}$", user):
+        print("Valid")
+        break
+    else:
+        print("Invalid")
+
+while True:
+    user = input("Enter a 3 or 4-digit number: ").strip()
+
+    if re.search(r"^[1234567890]{3,4}$", user):
+        print("Valid")
+        break
+    else:
+        print("Invalid")
+
+while True:
     number = input("Enter your phone number: ").strip()
 
     if re.search(r"^[0123456789]{4}-[0123456789]{3}-[0123456789]{4}$", number):
