88dc5ae2bb6bf21428133705f91873639fee4263
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-17 16:33:02 +0800

    Feat: add a guard for an Instagram username URL using re

--- week-7/programs/_01_scratch.py
+++ week-7/programs/_01_scratch.py
@@ -3,3 +3,12 @@
 import re
 
 
+# https://www.instagram.com/username/
+
+while True:
+    url = input("Enter your Instagram URL: ").strip()
+
+    if re.search(r"^(?:https?://|)(?:www\.|)instagram\.com/\w+/?$", url):
+        print("[Success:  System]")
+    else:
+        print("[Fail   :  System]")
