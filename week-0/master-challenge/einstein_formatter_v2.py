63d068fb00b5c8faa9e14b11634ee70f422a9166
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-18 21:48:06 +0800

    Refactor: add try/except for error case during mass input

--- week-0/master-challenge/einstein_formatter_v2.py
+++ week-0/master-challenge/einstein_formatter_v2.py
@@ -10,7 +10,17 @@ def main():
 def user_input():
     print("[Prompt  -  System] Please enter your name.")
     name = input("[User    -  System] ").strip()
-    return name
+
+    print("[Prompt  -  System] Using E = mc^2, enter a mass.")
+    while True:
+        user = input("[User    -  System] ").strip()
+        try:
+            mass = float(user)
+        except ValueError:
+            print("[Error   -  System] Invalid number.")
+        else:
+            break
+    return name, mass
 
 
 if __name__ == "__main__":
