64a8ecb43059e14904906d8f6e4d7da1bd4cc257
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-18 21:40:49 +0800

    Refactor: create initial structure with cleaner dialogue

--- week-0/master-challenge/einstein_formatter_v2.py
+++ week-0/master-challenge/einstein_formatter_v2.py
@@ -1,2 +1,17 @@
 # Document: A refactored version of Week-0's Master Challenge.
 
+def main():
+    print(
+        "\n[Message -  System] Greetings user!"
+    )
+    name = user_input()
+
+
+def user_input():
+    print("[Prompt  -  System] Please enter your name.")
+    name = input("[User    -  System] ").strip()
+    return name
+
+
+if __name__ == "__main__":
+    main()
