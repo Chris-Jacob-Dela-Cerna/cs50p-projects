bf31a3ef650243a8337a970544db8392d5215c71
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-19 20:18:17 +0800

    Feat: change a-z to a-f since hexadecimal only accept up to F

--- week-7/programs/_01_scratch.py
+++ week-7/programs/_01_scratch.py
@@ -31,7 +31,7 @@ def color_code():
     while True:
         user_code = input("Enter a hexadecimal color code: ").strip()
 
-        if code := re.search(r"^#([a-zA-Z0-9]{6})$", user_code):
+        if code := re.search(r"^#([a-fA-F0-9]{6})$", user_code):
             print(f"[Success:  System] Your color code is {code.group(1)}.")
         else:
             print("[Fail   :  System] Please enter a valid color code.")
