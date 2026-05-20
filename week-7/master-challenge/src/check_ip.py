3fe117f6ba4963ac97192f8928f28ef2288a2c6f
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-20 18:32:36 +0800

    Feat: add exit dialog and sys for sys.exit(

--- week-7/master-challenge/src/check_ip.py
+++ week-7/master-challenge/src/check_ip.py
@@ -1,12 +1,15 @@
 
 
 import re
+import sys
 
 
 def check_ip():
-    while True:
-        print("[Prompt - System] Enter IPv4 address.")
-        user_ip = input(">>> ").strip()
+    print("[Prompt - System] Enter IPv4 address.")
+    user_ip = input(">>> ").strip()
 
-        if re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", user_ip):
-            print("Valid")
+    if ip := re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", user_ip):
+        pass
+    else:
+        print("[Error  - System] Invalid IPv4 format.")
+        sys.exit()
