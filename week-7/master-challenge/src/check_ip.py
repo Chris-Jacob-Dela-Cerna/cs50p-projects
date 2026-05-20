b7a91b0b768c9d9c81a646521c25dd07684d6d7e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-20 18:29:54 +0800

    Feat: simplify re.search to strictly look for patterns instead of core logic

--- week-7/master-challenge/src/check_ip.py
+++ week-7/master-challenge/src/check_ip.py
@@ -6,9 +6,7 @@ import re
 def check_ip():
     while True:
         print("[Prompt - System] Enter IPv4 address.")
-        ip = input(">>> ").strip()
+        user_ip = input(">>> ").strip()
 
-        if re.search(r"^(((?:[0-9]){1})|((?:[1-9]){1}(?:[0-9]){1})|((?:[1]){1}(?:[0-9]){2})|((?:[2]){1}(?:[0-4]){1}(?:[0-9]){1})|((?:[2]){1}(?:[5]){1}(?:[0-5]){1}))$", ip):
+        if re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", user_ip):
             print("Valid")
-        else:
-            print("Invalid")
