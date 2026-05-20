f1a029fcde65f7d94d9b1834ddfd8479a3580f0f
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-20 17:32:22 +0800

    Feat: add match/case to check what sys mode is selected

--- week-7/master-challenge/main.py
+++ week-7/master-challenge/main.py
@@ -9,8 +9,15 @@ def main():
     except IndexError:
         print("[Error  - System] No modes selected.")
         sys.exit()
-
-    
+    match mode:
+        case "ipv4":
+            "check_ip()"
+        case "html":
+            "extract_html()"
+        case "24hour":
+            "convert_time()"
+        case "validate()":
+            "validate_email()"
 
 
 if __name__ == "__main__":
