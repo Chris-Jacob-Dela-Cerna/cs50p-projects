118b0be366c65af0359ec578f6ab522762ee715c
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-21 20:20:28 +0800

    Feat: add validate_number() to check if ip number range is correct

--- week-7/master-challenge/src/check_ip.py
+++ week-7/master-challenge/src/check_ip.py
@@ -5,11 +5,19 @@ import sys
 
 
 def check_ip():
-    print("[Prompt - System] Enter IPv4 address.")
+    print("[Prompt - System] Enter IPv4 address:")
     user_ip = input(">>> ").strip()
 
-    if ip := re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", user_ip):
-        pass
+    if ip := re.search(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", user_ip):
+        for number in ip.groups():
+            if not validate_number(number):
+                print(f"[Error  - System] Invalid IP number ({number}).")
     else:
         print("[Error  - System] Invalid IPv4 format.")
         sys.exit()
+
+
+def validate_number(number):
+    if 0 > int(number) or int(number) > 255:
+        return False
+    return True 
