1796f6ef7ac6827bb318cc3585929008b35a02dc
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-22 21:04:49 +0800

    Feat: print converted youtube html src to watchable url

--- week-7/master-challenge/src/extract_html.py
+++ week-7/master-challenge/src/extract_html.py
@@ -7,7 +7,7 @@ def extract_html():
     print("[Prompt - System] Enter Youtube HTML:")
     user_html = input(">>> ").strip()
 
-    if src := re.search(r"^<iframe .*src=\"https?://(?:www\.)?youtube.com/embed/(.{11})\" *.*></iframe>$", user_html):
-        print(src.group(1))
+    if src := re.search(r"^<iframe .*src=\"(https?://(?:www\.)?)youtube.com/embed/(.{11})\" *.*></iframe>$", user_html):
+        print(f"[Success - System] {src.group(1)}youtu.be/{src.group(2)}")
     else:
         print("[Error  - System] Invalid HTML format.")
