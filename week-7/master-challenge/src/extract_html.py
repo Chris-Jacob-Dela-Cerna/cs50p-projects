205c792bbdaf065a4e44260f978f031a9d8103bc
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-22 20:45:25 +0800

    Feat: make src url more flexible for re.search

--- week-7/master-challenge/src/extract_html.py
+++ week-7/master-challenge/src/extract_html.py
@@ -7,5 +7,7 @@ def extract_html():
     print("[Prompt - System] Enter Youtube HTML:")
     user_html = input(">>> ").strip()
 
-    if src := re.search("^<iframe .*src=\"https://www.youtube.com/embed/(.{11})\" *.*></iframe>$", user_html):
+    if src := re.search(r"^<iframe .*src=\"https?://(?:www\.)?youtube.com/embed/(.{11})\" *.*></iframe>$", user_html):
         print(src.group(1))
+    else:
+        print("[Error  - System] Invalid HTML format.")
