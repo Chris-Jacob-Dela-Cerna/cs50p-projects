e381b124a8e0c00ccd8fd91e13a5e25144e67f00
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-22 20:15:49 +0800

    Feat: create a successfull regex for filtering in the youtube src embed

--- week-7/master-challenge/src/extract_html.py
+++ week-7/master-challenge/src/extract_html.py
@@ -1,4 +1,11 @@
 
 
+import re
+
+
 def extract_html():
-    pass
+    print("[Prompt - System] Enter Youtube HTML:")
+    user_html = input(">>> ").strip()
+
+    if src := re.search("^<iframe .*src=\"https://www.youtube.com/embed/(.{11})\" *.*></iframe>$", user_html):
+        print(src.group(1))
