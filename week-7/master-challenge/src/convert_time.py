cbd239889f5071fd2875ba46e6b7dfbda9748a59
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-22 21:56:54 +0800

    Feat: add time filter for re.search in convert_time

--- week-7/master-challenge/src/convert_time.py
+++ week-7/master-challenge/src/convert_time.py
@@ -4,4 +4,8 @@ import re
 
 
 def convert_time():
-    pass
+    print("[Prompt - System] Enter time:")
+    user_time = input(">>> ").strip()
+
+    if time := re.search(r"^([0-9](?::[0-9]{2})) to ([0-9](?::[0-9]{2}))$", user_time):
+        print(time.group(1), time.group(2))
