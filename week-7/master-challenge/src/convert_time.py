6a46296ab75780eb71be8bd3347feda2fe80a9be
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-23 13:37:16 +0800

    Feat: seperate hour/minute checking into functions

--- week-7/master-challenge/src/convert_time.py
+++ week-7/master-challenge/src/convert_time.py
@@ -7,7 +7,7 @@ from utils.filter import filter_
 def convert_time():
     print("[Prompt - System] Enter time:")
     input_ = input(">>> ").strip()
-    condition = r"^([0-9](?::[0-9]{2})?) (AM|PM) to ([0-9](?::[0-9]{2})?) (AM|PM)$"
+    condition = r"^([0-9]{1,2}(?::[0-9]{2})?) (AM|PM) to ([0-9]{1,2}(?::[0-9]{2})?) (AM|PM)$"
 
     if not (time_list := filter_(condition, input_)):
         abort("Invalid time format.")
@@ -15,10 +15,10 @@ def convert_time():
     hour1, minute1 = extract_time(time_list[0])
     hour2, minute2 = extract_time(time_list[2])
 
-    if 1 > hour1 > 12 or 1 > hour2 > 12:
-        abort("Invalid hour range.")
-    if minute1 > 59 or minute2 > 59:
-        abort("Invalid minute range.")
+    check_hour(hour1)
+    check_hour(hour2)
+    check_minute(minute1)
+    check_minute(minute2)
 
     period1 = time_list[1]
     period2 = time_list[3]
@@ -29,3 +29,13 @@ def extract_time(time):
         hour, minute = time.split(":")
         return int(hour), int(minute)
     return int(time), 0
+
+
+def check_hour(hour):
+    if hour < 1 or hour > 12:
+        abort(f"Invalid hour range ({hour}).")
+
+
+def check_minute(minute):
+    if minute > 59:
+        abort(f"Invalid minute range ({minute}).")
