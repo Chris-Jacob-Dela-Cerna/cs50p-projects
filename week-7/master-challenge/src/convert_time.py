e1e78df8bb02da0c3e54f7aca22e59fb5e38830c
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-23 11:34:49 +0800

    Feat: accept AM and PM for time

--- week-7/master-challenge/src/convert_time.py
+++ week-7/master-challenge/src/convert_time.py
@@ -7,7 +7,7 @@ def convert_time():
     print("[Prompt - System] Enter time:")
     user_time = input(">>> ").strip()
 
-    if time := re.search(r"^([0-9](?::[0-9]{2})?) to ([0-9](?::[0-9]{2})?)$", user_time):
-        print(time.group(1), time.group(2))
+    if time := re.search(r"^([0-9](?::[0-9]{2})?) (AM|PM) to ([0-9](?::[0-9]{2})?) (AM|PM)$", user_time):
+        print(time.groups())
     else:
         print("[Error  - System] Invalid time format.")
