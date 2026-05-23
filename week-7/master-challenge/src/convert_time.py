10c0e70723b5fa5d0e70698251a29145692226c8
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-23 13:51:46 +0800

    Feat: clean up code structure and merge hour and minute into check_time

--- week-7/master-challenge/src/convert_time.py
+++ week-7/master-challenge/src/convert_time.py
@@ -14,21 +14,13 @@ def convert_time():
 
     hour1, minute1 = extract_time(time_list[0])
     hour2, minute2 = extract_time(time_list[2])
-
-    check_hour(hour1)
-    check_hour(hour2)
-    check_minute(minute1)
-    check_minute(minute2)
+    check_time(hour1, minute1)
+    check_time(hour2, minute2)
 
     period1 = time_list[1]
     period2 = time_list[3]
-
-    if period1 == "PM":
-        hour1 *= 2
-    if period2 == "PM":
-        hour2 *= 2
-    
-    print(hour1, hour2)
+    hour1 = check_meridiem(period1, hour1)
+    hour2 = check_meridiem(period2, hour2)
 
 
 def extract_time(time):
@@ -38,11 +30,12 @@ def extract_time(time):
     return int(time), 0
 
 
-def check_hour(hour):
+def check_time(hour, minute):
     if hour < 1 or hour > 12:
         abort(f"Invalid hour range ({hour}).")
-
-
-def check_minute(minute):
     if minute > 59:
         abort(f"Invalid minute range ({minute}).")
+
+
+def check_meridiem(period, hour):
+    pass
