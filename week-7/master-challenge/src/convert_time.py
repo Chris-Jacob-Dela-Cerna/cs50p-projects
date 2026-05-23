729d27dd5dc9418cb00974a6714874291d184123
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-23 11:28:14 +0800

    Feat: add ? for optional filters

--- week-7/master-challenge/src/convert_time.py
+++ week-7/master-challenge/src/convert_time.py
@@ -7,5 +7,7 @@ def convert_time():
     print("[Prompt - System] Enter time:")
     user_time = input(">>> ").strip()
 
-    if time := re.search(r"^([0-9](?::[0-9]{2})) to ([0-9](?::[0-9]{2}))$", user_time):
+    if time := re.search(r"^([0-9](?::[0-9]{2})?) to ([0-9](?::[0-9]{2})?)$", user_time):
         print(time.group(1), time.group(2))
+    else:
+        print("[Error  - System] Invalid time format.")
