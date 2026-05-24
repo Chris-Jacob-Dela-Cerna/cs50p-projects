a6cce6000b1d099b12096e46215ca14fca301d23
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-24 22:56:19 +0800

    Feat: use dispatcher for main.py

--- week-7/master-challenge/main.py
+++ week-7/master-challenge/main.py
@@ -14,19 +14,18 @@ def main():
     except IndexError:
         print("[Error  - System] No modes selected.")
         sys.exit(1)
-    match mode:
-        case "ipv4":
-            check_ip()
-        case "24hour":
-            convert_time()
-        case "countum":
-            count_um()
-        case "html":
-            extract_html()
-        case "validate":
-            validate_email()
-        case _:
-            print("[Error  - System] Invalid mode.")
+
+    modes = {
+        "ipv4": check_ip,
+        "24hour": convert_time,
+        "countum": count_um,
+        "html": extract_html,
+        "validate": validate_email,
+    }
+
+    if mode not in modes:
+        sys.exit(1)
+    modes[mode]()
 
 
 if __name__ == "__main__":
