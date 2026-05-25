f24bf3eb89aede21e153f20e9407d62bcb52a9bf
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-25 14:55:59 +0800

    Feat: add final unit tests with filter_convert_time()

--- week-7/master-challenge/tests/test_convert_time.py
+++ week-7/master-challenge/tests/test_convert_time.py
@@ -2,6 +2,7 @@
 
 from src.convert_time import convert_hour
 from src.convert_time import extract_time
+from utils.filter import filter_
 
 
 def test_convert_hour():
@@ -14,3 +15,12 @@ def test_convert_hour():
 def test_extract_time():
     assert extract_time("9:30") == (9, 30)
     assert extract_time("9") == (9, 0)
+    
+
+def test_filter_convert_time():
+    condition = r"^([0-9]{1,2}(?::[0-9]{2})?) (AM|PM) to ([0-9]{1,2}(?::[0-9]{2})?) (AM|PM)$"
+    assert filter_(condition, "9:30 to 5:30") == None
+    assert filter_(condition, "9:30 AM - 5:30 PM") == None
+    assert filter_(condition, "9 30 AM to 5 30 PM") == None
+    assert filter_(condition, "9:30 am to 5:30 pm") == None
+    assert filter_(condition, "9:30 AM to 5:30 PM").groups() == ("9:30", "AM", "5:30", "PM")
