da4ceb446ad57858ca67e10f918909ef489ff574
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-25 20:10:36 +0800

    Feat: use try/except to capture error with invalid input

--- week-8/programs/_01_scratch.py
+++ week-8/programs/_01_scratch.py
@@ -1,18 +1,28 @@
 
 
+import sys
+
+
 class Fruits:
-    ...
+    def __init__(self, name, color):
+        if not name or not color:
+            raise ValueError
+        self.name = name
+        self.color = color
 
 
 def main():
     fruit = get_fruits()
-    print(fruit.name, fruit.color)
+    print(f"[Succeed - System] {fruit.name} is {fruit.color}.")
 
 
 def get_fruits():
-    name = input("Enter fruit: ").strip()
-    color = input("Enter color: ").strip()
-    return Fruits(name, color)
+    name = input("[Prompt - System] Fruit: ").strip()
+    color = input("[Prompt - System] Color: ").strip()
+    try:
+        return Fruits(name, color)
+    except ValueError:
+        sys.exit("[Error  - System] Invalid input.")
 
 
 
