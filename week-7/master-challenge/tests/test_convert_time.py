c2f344e983385a3cfee7159cda726155e8e56d5b
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-25 14:50:52 +0800

    Feat: add unit tests for convert_hour()

--- week-7/master-challenge/tests/test_convert_time.py
+++ week-7/master-challenge/tests/test_convert_time.py
@@ -1,6 +1,14 @@
 
 
+from src.convert_time import convert_hour
 from src.convert_time import extract_time
+
+
+def test_convert_hour():
+    assert convert_hour("AM", 6) == 6
+    assert convert_hour("AM", 12) == 0
+    assert convert_hour("PM", 6) == 18
+    assert convert_hour("PM", 12) == 24
 
 
 def test_extract_time():
