c3bdb465ebeea8c75900b8c1d9d6fc2d8d53a46e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-25 14:32:29 +0800

    Feat: add final unit tests for filter_ in check_ip()

--- week-7/master-challenge/tests/test_check_ip.py
+++ week-7/master-challenge/tests/test_check_ip.py
@@ -1,8 +1,20 @@
 
 
 from src.check_ip import check_ip_number
+from utils.filter import filter_
 
 
 def test_check_ip_number():
     assert check_ip_number(["0", "0", "0", "256"]) == False
+    assert check_ip_number(["0", "0", "0", "0"]) == True
     assert check_ip_number(["0", "55", "155", "255"]) == True
+
+
+def test_filter_check_ip():
+    condition = r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$"
+    assert filter_(condition, "0 0 0 0") == None
+    assert filter_(condition, "0.0.0") == None
+    assert filter_(condition, "0.0.0.0.0") == None
+    assert filter_(condition, "0.0.0.-1") == None
+    assert filter_(condition, "0.0.0.0").groups() == ("0", "0", "0", "0")
+    assert filter_(condition, "0.55.155.255").groups() == ("0", "55", "155", "255")
