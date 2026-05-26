8688f47721587672f94eb9061720ccf5665c65f5
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-26 20:50:55 +0800

    Feat: add initial input to class for email in main()

--- week-8/programs/_02_email.py
+++ week-8/programs/_02_email.py
@@ -4,11 +4,25 @@ import re
 
 
 class Email:
-    ...
+    def __init__(self, email):
+        self.email = email
+
+
+class Account:
+    def __init__(self, username, password):
+        ...
 
 
 def main():
-    ...
+    input(
+        "\n   ============"
+        "\n     Sign-up"
+        "\n   ============"
+        "\n"
+    )
+    print("[Prompt - System] Enter your email:")
+    email = Email(input(">>> ").strip())
+    print(email.email)
 
 
 if __name__ == "__main__":
