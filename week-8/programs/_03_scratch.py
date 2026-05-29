cd634c21ed7403634a09d3ed3016edc4fa8db743
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-29 20:40:53 +0800

    Try: experiment with class variables

---
+++ week-8/programs/_03_scratch.py
@@ -0,0 +1,28 @@
+# OOP scratch file 2 - practicing classes and objects
+
+
+class Horse:
+    health = 5
+    attack = 2
+    defence = 2
+    min_movement = 1
+    max_movement = 2
+
+    @classmethod
+    def move(cls, movement):
+        if movement < cls.min_movement:
+            raise ValueError("Too small.")
+        if movement > cls.max_movement:
+            raise ValueError("Too big.")
+        print(f"Horse has moved {movement} tiles.")
+
+try:
+    Horse.move(1)
+except ValueError as ve:
+    print(ve)
+
+horse = Horse()
+try:
+    horse.move(2)
+except ValueError as ve:
+    print(ve)
