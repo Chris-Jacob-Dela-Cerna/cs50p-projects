c80226e405c74658f37237eec1d3b9e1033e10ba
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-30 00:32:30 +0800

    Feat: add initial class traits for Warrior

---
+++ week-8/programs/_04_battle.py
@@ -0,0 +1,20 @@
+
+
+class Warrior:
+    traits = {
+        "normal": {
+            "health": 10,
+            "defence": 2,
+            "attack": 2,
+        },
+        "tanky": {
+            "health": 15,
+            "defence": 3,
+            "attack": 1,
+        },
+        "agile": {
+            "health": 10,
+            "defence": 1,
+            "attack": 3,
+        },
+    }
