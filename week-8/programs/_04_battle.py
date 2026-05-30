76afa6335c98a4a171fad9adb5678940ad14c797
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-30 13:35:12 +0800

    Feat: add Archer and Giant class with their own stats and traits

--- week-8/programs/_04_battle.py
+++ week-8/programs/_04_battle.py
@@ -38,12 +38,90 @@ class Warrior:
     # Shows a unit's stats
     def stats(self):
         return f"\nTrait: {self._trait.title()}\nHealth: {self._health}\nDefence: {self._defence}\nAttack: {self._attack}"
+    
+
+class Archer:
+    traits = {
+        "normal": {
+            "health": 120,
+            "attack": 25,
+            "defence": 15,
+        },
+        "sniper": {
+            "health": 80,
+            "attack": 45,
+            "defence": 5,
+        },
+        "evasive": {
+            "health": 110,
+            "attack": 20,
+            "defence": 18,
+        },
+    }
+
+    def __init__(self, trait):
+        if not trait:
+            raise ValueError("No trait selected.")
+        if trait not in Archer.traits:
+            raise ValueError("Invalid trait.")
+        self._trait = trait
+
+        self._health = Archer.traits[trait]["health"]
+        self._defence = Archer.traits[trait]["defence"]
+        self._attack = Archer.traits[trait]["attack"]
+
+    # Runs once when setting the unit's trait
+    @property
+    def trait(self):
+        return self._trait
+
+    # Shows a unit's stats
+    def stats(self):
+        return f"\nTrait: {self._trait.title()}\nHealth: {self._health}\nDefence: {self._defence}\nAttack: {self._attack}"
+    
+
+class Giant:
+    traits = {
+        "normal": {
+            "health": 160,
+            "attack": 28,
+            "defence": 15,
+        },
+        "brawler": {
+            "health": 140,
+            "attack": 40,
+            "defence": 8,
+        },
+        "colossus": {
+            "health": 200,
+            "attack": 15,
+            "defence": 22,
+        },
+    }
+
+    def __init__(self, trait):
+        if not trait:
+            raise ValueError("No trait selected.")
+        if trait not in Giant.traits:
+            raise ValueError("Invalid trait.")
+        self._trait = trait
+
+        self._health = Giant.traits[trait]["health"]
+        self._defence = Giant.traits[trait]["defence"]
+        self._attack = Giant.traits[trait]["attack"]
+
+    # Runs once when setting the unit's trait
+    @property
+    def trait(self):
+        return self._trait
+
+    # Shows a unit's stats
+    def stats(self):
+        return f"\nTrait: {self._trait.title()}\nHealth: {self._health}\nDefence: {self._defence}\nAttack: {self._attack}"
 
 
 def main():
-    w1 = Warrior("normal")
-    w2 = Warrior("tanky")
-    w3 = Warrior("agile")
+    ...
 
 
 if __name__ == "__main__":
