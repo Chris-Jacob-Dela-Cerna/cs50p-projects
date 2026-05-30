48eb434ae1a5e6a0ec511ab058064e6cfd13999d
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-30 11:25:32 +0800

    Feat: shorten code and remove setter for trait

--- week-8/programs/_04_battle.py
+++ week-8/programs/_04_battle.py
@@ -20,38 +20,31 @@ class Warrior:
     }
 
     def __init__(self, trait):
-        self.traits = Warrior.traits
-        self.trait = trait
-        self._health = self.traits[trait]["health"]
-        self._defence = self.traits[trait]["defence"]
-        self._attack = self.traits[trait]["attack"]
-        self._initialized = True
-
-    @property
-    def trait(self):
-        return self._trait
-    
-    @trait.setter
-    def trait(self, trait):
-        if hasattr(self, "_initialized") and self._initialized:
-            raise AttributeError("Trait cannot be changed.")
         if not trait:
             raise ValueError("No trait selected.")
         if trait not in Warrior.traits:
             raise ValueError("Invalid trait.")
         self._trait = trait
+        self.trait = trait
+        self._health = Warrior.traits[trait]["health"]
+        self._defence = Warrior.traits[trait]["defence"]
+        self._attack = Warrior.traits[trait]["attack"]
 
+    # Runs once when setting the unit's trait
+    @property
+    def trait(self):
+        return self._trait
+
+    # Shows a unit's stats
     def stats(self):
         return f"\nTrait: {self._trait.title()}\nHealth: {self._health}\nDefence: {self._defence}\nAttack: {self._attack}"
 
 
 def main():
     w1 = Warrior("normal")
-    print(w1.stats())
     w2 = Warrior("tanky")
-    print(w2.stats())
     w3 = Warrior("agile")
-    print(w3.stats())
+    print(w1.stats())
 
 
 if __name__ == "__main__":
