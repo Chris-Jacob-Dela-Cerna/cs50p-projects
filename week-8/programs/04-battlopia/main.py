52cdb25ed7b966ebb78285200df681745874d029
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-01 13:03:54 +0800

    Feat: create ui folder to move show_ functions

---
+++ week-8/programs/04-battlopia/main.py
@@ -0,0 +1,13 @@
+
+
+from ui import intro
+
+
+def main():
+    input("[Message - System] Press enter between pauses. ")
+    intro.show_greet()
+    intro.show_instructions()
+
+
+if __name__ == "__main__":
+    main()
---
+++ week-8/programs/04-battlopia/ui/intro.py
@@ -0,0 +1,25 @@
+
+
+def show_greet():
+    input(
+        "================="
+        "\n Greetings, User"
+        "\n================="
+        "\n"
+    )
+    input(
+        "=========================="
+        "\n   Welcome to Battlopia!"
+        "\n A Text-based Battle Game"
+        "\n=========================="
+        "\n"
+    )
+
+
+def show_instructions():
+    input(
+        "Note: This game requires 2 players."
+        "\n      Each player must select 3 units for their deck."
+        "\n      The player with the last remaining unit/s wins."
+        "\n"
+    )
