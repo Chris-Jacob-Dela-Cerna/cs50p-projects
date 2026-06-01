2018ec6f000d11634120ff73ba92d589694a5a78
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-01 11:08:59 +0800

    Feat: rename battle.py to battlopia, and add entrace message

---
+++ week-8/programs/04-battlopia/_04_battlopia.py
@@ -0,0 +1,30 @@
+
+
+from units import Warrior
+from units import Archer
+from units import Giant
+
+
+def main():
+    input(
+        "\n================="
+        "\n Greetings, User"
+        "\n================="
+        "\n"
+    )
+    input(
+        "\n========================"
+        "\n Welcome to Battlopia "
+        "\n========================"
+        "\n"
+    )
+    input(
+        "\n=========================="
+        "\n A Text-based Battle Game "
+        "\n=========================="
+        "\n"
+    )
+
+
+if __name__ == "__main__":
+    main()
