5f55b9ff890364a63a05626e79e048e766f64aa7
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-03 19:34:40 +0800

    Feat: add the rest of inflect logic to print number to words

--- week-8/master-challenge/main.py
+++ week-8/master-challenge/main.py
@@ -1,12 +1,17 @@
 
 
 import datetime as dt
+import inflect
 
 
 birth_date = dt.date(2008, 5, 2)
 today = dt.date.today()
 age = today - birth_date
 days = dt.timedelta(days=6606)
-minutes = days.total_seconds() / 60
+minutes = round(days.total_seconds() / 60)
 
 print(minutes)
+
+p = inflect.engine()
+
+print(p.number_to_words(minutes, andword="").capitalize())
