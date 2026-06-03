004fa2c10310e960e7b93d27815cb2541e4d2dec
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-03 22:08:58 +0800

    Feat: add birth_date validator to extract year/month\day

--- week-8/master-challenge/src/age.py
+++ week-8/master-challenge/src/age.py
@@ -1,8 +1,11 @@
 
 
+import re
+import sys
 from utils.get_sys import get_sys
 
 
 def age():
     birth_date = get_sys(2, "[Error  - System] Please enter a birthdate in this format: YYYY-MM-DD")
-
+    if not (bd := re.search(r"^([0-9]{4})-([0-9]{2})-([0-9]{2})$", birth_date)):
+        sys.exit("[Error  - System] Please use the right format: YYYY-MM-DD")
