f4c0670ead4308048273b4a9aae79236119f4070
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-03 20:06:57 +0800

    Feat: add deposit instance method to store cookies and check if invalid amount

--- week-8/master-challenge/main.py
+++ week-8/master-challenge/main.py
@@ -17,9 +17,23 @@ class Jar:
             raise ValueError("A cookie jar can accept more cookies than that.")
         if not isinstance(capacity, int):
             if not isinstance(capacity, float):
-                raise TypeError("A quantity must be a number.")
+                raise ValueError("A quantity must be a number.")
         if capacity < 1:
             raise ValueError("A cookie jar can't be negatively full.")
         self._capacity = capacity
 
-print(Jar(1.5).capacity)
+    def deposit(self, cookies):
+        if not cookies:
+            raise ValueError("You can't deposit zero cookies.")
+        if not isinstance(cookies, int):
+            if not isinstance(cookies, float):
+                raise TypeError("A quantity must be a number.")
+        if cookies < 0:
+            raise ValueError("Negative cookies isn't a thing.")
+        if cookies > self._capacity:
+            raise ValueError("Cookie overflow!")
+        self._cookies = cookies
+
+jar = Jar(3)
+print(jar.capacity)
+jar.deposit("l")
