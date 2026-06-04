c2b7ba1b3ef389f11f8e6fae504af0ed8c6d26c8
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-04 20:20:04 +0800

    Feat: add data folder to store cookie jar class data

---
+++ week-8/master-challenge/data/cookie_jar.py
@@ -0,0 +1,40 @@
+
+
+class Jar:
+    cookies = 0
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self._cookies = Jar.cookies
+
+    def __str__(self):
+        cookies = ""
+        for _ in range(self._cookies):
+            cookies += "🍪"
+        return f"Cookies: {cookies}"
+
+    @property
+    def capacity(self):
+        return self._capacity
+    
+    @capacity.setter
+    def capacity(self, capacity):
+        if not capacity:
+            raise ValueError("A cookie jar can accept more cookies than that.")
+        if not isinstance(capacity, int):
+            if not isinstance(capacity, float):
+                raise ValueError("A quantity must be a number.")
+        if capacity < 1:
+            raise ValueError("A cookie jar can't be negatively full.")
+        self._capacity = capacity
+
+    def deposit(self, cookies):
+        if not cookies:
+            raise ValueError("You can't deposit zero cookies.")
+        if not isinstance(cookies, int):
+            if not isinstance(cookies, float):
+                raise TypeError("A quantity must be a number.")
+        if cookies < 0:
+            raise ValueError("Negative cookies isn't a thing.")
+        if cookies > self._capacity:
+            raise ValueError("Cookie overflow!")
+        self._cookies = cookies
