5d9230089a4125505833b5252bef0305114b25b9
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-06 21:40:21 +0800

    Feat: add initial structure for sys calculator

--- week-9/programs/_03_sys_calculator.py
+++ week-9/programs/_03_sys_calculator.py
@@ -4,9 +4,31 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--add", type=int, nargs=2, help="Add 2 integers")
-parser.add_argument("--subtract", type=int, nargs=2, help="Subtract 2 integers")
-parser.add_argument("--multiply", type=int, nargs=2, help="Multiply 2 integers")
-parser.add_argument("--divide", type=int, nargs=2, help="Divide 2 integers")
 
-args = parser.parse_args()
+def main():
+    global args
+    parser.add_argument("--add", type=int, nargs=2, help="Add 2 integers")
+    parser.add_argument("--subtract", type=int, nargs=2, help="Subtract 2 integers")
+    parser.add_argument("--multiply", type=int, nargs=2, help="Multiply 2 integers")
+    parser.add_argument("--divide", type=int, nargs=2, help="Divide 2 integers")
+    args = parser.parse_args()
+
+
+def addition():
+    print("add")
+
+
+def subtraction():
+    print("subtract")
+
+
+def multiply():
+    print("multiply")
+
+
+def divide():
+    print("divide")
+
+
+if __name__ == "__main__":
+    main()
