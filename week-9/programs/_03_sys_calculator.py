281d5b2a8bc06f63a37bafda295f5ff4241cf6b9
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-07 10:43:06 +0800

    Feat: move parse_args to main and turn into global variable

--- week-9/programs/_03_sys_calculator.py
+++ week-9/programs/_03_sys_calculator.py
@@ -7,10 +7,11 @@ parser.add_argument("--add", type=int, nargs=2, help="Add 2 integers")
 parser.add_argument("--subtract", type=int, nargs=2, help="Subtract 2 integers")
 parser.add_argument("--multiply", type=int, nargs=2, help="Multiply 2 integers")
 parser.add_argument("--divide", type=int, nargs=2, help="Divide 2 integers")
-args = parser.parse_args()
 
 
 def main():
+    global args
+    args = parser.parse_args()
     x = "Enter an operation."
     if args.add != None:
         x = addition()
