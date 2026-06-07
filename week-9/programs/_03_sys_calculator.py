0596acce77bce727302c2ed3f2914f6da06cbced
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-06-07 10:41:50 +0800

    Feat: add logic for simple sys calculator

--- week-9/programs/_03_sys_calculator.py
+++ week-9/programs/_03_sys_calculator.py
@@ -3,31 +3,40 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--add", type=int, nargs=2, help="Add 2 integers")
+parser.add_argument("--subtract", type=int, nargs=2, help="Subtract 2 integers")
+parser.add_argument("--multiply", type=int, nargs=2, help="Multiply 2 integers")
+parser.add_argument("--divide", type=int, nargs=2, help="Divide 2 integers")
+args = parser.parse_args()
 
 
 def main():
-    global args
-    parser.add_argument("--add", type=int, nargs=2, help="Add 2 integers")
-    parser.add_argument("--subtract", type=int, nargs=2, help="Subtract 2 integers")
-    parser.add_argument("--multiply", type=int, nargs=2, help="Multiply 2 integers")
-    parser.add_argument("--divide", type=int, nargs=2, help="Divide 2 integers")
-    args = parser.parse_args()
+    x = "Enter an operation."
+    if args.add != None:
+        x = addition()
+    elif args.subtract != None:
+        x = subtraction()
+    elif args.multiply != None:
+        x = multiplication()
+    elif args.divide != None:
+        x = division()
+    print(x)
 
 
 def addition():
-    print("add")
+    return args.add[0] + args.add[1]
 
 
 def subtraction():
-    print("subtract")
+    return args.subtract[0] - args.subtract[1]
 
 
-def multiply():
-    print("multiply")
+def multiplication():
+    return args.multiply[0] * args.multiply[1]
 
 
-def divide():
-    print("divide")
+def division():
+    return args.divide[0] / args.divide[1]
 
 
 if __name__ == "__main__":
